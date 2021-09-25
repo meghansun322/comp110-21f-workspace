b5bada4d906d347f6e3c702b5e7f98e2e9263a6d
Meghan Sun <mlsun@ad.unc.edu>
2021-09-25 17:53:31 -0400

    Completed exercise04!

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -3,4 +3,41 @@
 __author__ = "123456789"
 
 
-# TODO: Implement your functions here.
+def all(list: list[int], match_number: int) -> bool:
+    """Returns whether all integer match the desired integer."""
+    if len(list) == 0:
+        return False
+    i: int = 0
+    while i < len(list):
+        if(list[i] != match_number):
+            return False
+        i = i + 1
+    return True
+
+
+def is_equal(list1: list[int], list2: list[int]) -> bool:
+    """Returns whether two lists are equal."""
+    if(len(list1) != len(list2)):
+        return False
+
+    i: int = 0
+    while i < len(list1):
+        if(list1[i] != list2[i]):
+            return False
+        i = i + 1
+
+    return True
+
+
+def max(list: list[int]) -> int:
+    """Returns the max value in a list."""
+    max: int = list[0]
+    if len(list) == 0:
+        raise ValueError("max() arg is an empty List")
+    i: int = 0
+    while i < len(list):
+        if list[i] > max:
+            max = list[i]
+        i = i + 1
+
+    return max
