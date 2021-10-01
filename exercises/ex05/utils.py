c70472f66ac9b40a44c19c3d88107f271680bbe4
Meghan Sun <mlsun@ad.unc.edu>
2021-10-01 11:09:21 -0400

    Completed ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -2,4 +2,38 @@
 
 __author__ = "123456789"
 
-# Define your functions below
+
+def only_evens(nums: list[int]) -> list[int]:
+    """Keep the evens."""
+    evens: list[int] = []
+    for number in nums:
+        if number % 2 == 0:
+            evens.append(number)
+    return evens
+
+
+def sub(nums: list[int], start: int, end: int) -> list[int]:
+    """Return list of numbers from desired index range."""
+    if(len(nums) == 0 or start > len(nums) or end <= 0):
+        return []
+
+    if(start < 0):
+        start = 0
+
+    if(end > len(nums)):
+        end = len(nums)
+
+    new_list: list[int] = []
+    i: int = start
+    while i < end:
+        new_list.append(nums[i])
+        i = i + 1
+    return new_list
+
+
+def concat(list_1: list[int], list_2: list[int]) -> list[int]:
+    """Return combined list of numbers."""
+    concat_list: list[int] = list_1
+    for numbers in list_2:
+        concat_list.append(numbers)
+    return concat_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -4,3 +4,85 @@
 # from exercises.ex05.utils import only_evens, sub, concat
 
 __author__ = "123456789"
+
+from exercises.ex05.utils import only_evens, sub, concat
+
+
+def test_only_evens_use_case00() -> None:
+    x: list[int] = [1, 2, 3]
+    assert only_evens(x) == [2]
+
+
+def test_only_evens_use_case01() -> None:
+    x: list[int] = [4, 4, 4] 
+    assert only_evens(x) == [4, 4, 4]
+
+
+def test_only_evens_no_even() -> None:
+    x: list[int] = [1, 5, 3]
+    assert only_evens(x) == []
+
+
+def test_sub_use_case00() -> None:
+    x: list[int] = [10, 20, 30, 40]
+    assert sub(x, 1, 3) == [20, 30]
+
+
+def test_sub_use_case01() -> None:
+    x: list[int] = [10, 20, 30, 40]
+    assert sub(x, 0, 3) == [10, 20, 30]
+
+
+def test_sub_start_neg() -> None:
+    x: list[int] = [10, 20, 30, 40]
+    assert sub(x, -1, 3) == [10, 20, 30]
+
+
+def test_sub_end_greater_len() -> None:
+    x: list[int] = [10, 20, 30, 40]
+    assert sub(x, 1, 9) == [20, 30, 40]    
+
+
+def test_sub_start_greater_length() -> None:
+    x: list[int] = [10, 20, 30, 40]
+    assert sub(x, 6, 7) == []
+
+
+def test_sub_list_is_0() -> None:
+    x: list[int] = []
+    assert sub(x, 6, 7) == []
+
+
+def test_sub_end_not_pos() -> None:
+    x: list[int] = [10, 20]
+    assert sub(x, 6, -1) == []
+
+
+def test_concat_use_case00() -> None:
+    x: list[int] = [10, 20]
+    y: list[int] = [30, 40]
+    assert concat(x, y) == [10, 20, 30, 40]
+
+
+def test_concat_use_case01() -> None:
+    x: list[int] = [10]
+    y: list[int] = [20, 30, 40]
+    assert concat(x, y) == [10, 20, 30, 40]
+
+
+def test_concat_list1_empty() -> None:
+    x: list[int] = []
+    y: list[int] = [20, 30, 40]
+    assert concat(x, y) == [20, 30, 40]
+
+
+def test_concat_list2_empty() -> None:
+    x: list[int] = []
+    y: list[int] = [20, 30, 40]
+    assert concat(y, x) == [20, 30, 40]
+
+
+def test_concat_both_empty() -> None:
+    x: list[int] = []
+    y: list[int] = []
+    assert concat(x, y) == []
