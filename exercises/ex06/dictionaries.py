6eebc16578b1ad6393446ccad6dc52cfbc7af8c6
Meghan Sun <mlsun@ad.unc.edu>
2021-10-27 20:54:29 -0400

    EX06!

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -2,4 +2,47 @@
 
 __author__ = "123456789"
 
-# Define your functions below
+
+def invert(dictionary: dict[str, str]) -> dict[str, str]:
+    """Given a dictionary, returns a dictionary that inverts key and value."""
+    invert_dict: dict[str, str] = dict()
+    for key in dictionary:
+        if dictionary[key] in invert_dict:
+            raise KeyError("Dictionary contains multiple identical values")
+        else:
+            invert_dict[dictionary[key]] = key
+    return invert_dict
+
+
+def favorite_color(dictionary: dict[str, str]) -> str:
+    """Returns the color of most popular color given a dictionary."""
+    favorite: str = ""
+    color_tracker: dict[str, int] = dict()
+    for key in dictionary:
+        if dictionary[key] in color_tracker:
+            color_tracker[dictionary[key]] = color_tracker[dictionary[key]] + 1
+        else:
+            color_tracker[dictionary[key]] = 1
+
+    highest: int = 0
+    for key in color_tracker:
+        if color_tracker[key] > highest:
+            highest = color_tracker[key]
+            favorite = key
+
+    if (favorite == ""):
+        return "no colors found"
+    else:
+        return favorite
+
+
+def count(counter: list[str]) -> dict[str, int]:
+    """Returns a dictionary of the counts of values for each item of input."""
+    dictionary: dict[str, int] = dict()
+
+    for value in counter:
+        if value in dictionary:
+            dictionary[value] += 1
+        else:
+            dictionary[value] = 1
+    return dictionary
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,87 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
+import pytest
 
 __author__ = "123456789"
+
+
+def test_invert_use_case_00() -> None:
+    """Test."""
+    d: dict[str, str] = {
+        'a': 'z',
+        'b': 'y',
+        'c': 'x'
+    }
+    assert invert(d) == {'z': 'a', 'y': 'b', 'x': 'c'}
+
+
+def test_invert_use_case_01() -> None:
+    """Test."""
+    d: dict[str, str] = {'apple': 'cat'}
+    assert invert(d) == {'cat': 'apple'}
+
+
+def test_invert_same_key() -> None:
+    """Test."""
+    d: dict[str, str] = {'kris': 'jordan', 'michael': 'jordan'}
+    with pytest.raises(KeyError):
+        invert(d)
+
+
+def test_invert_use_empty() -> None:
+    """Test."""
+    d: dict[str, str] = {}
+    assert invert(d) == {}
+
+
+def test_favorite_color_use_case_00() -> None:
+    """Test."""
+    d: dict[str, str] = {"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}
+    assert favorite_color(d) == "blue"
+
+
+def test_favorite_color_use_case_01() -> None:
+    """Test."""
+    d: dict[str, str] = {"Marc": "yellow", "Ezri": "red", "Kris": "blue"}
+    assert favorite_color(d) == "yellow"
+
+
+def test_favorite_color_use_case_02() -> None:
+    """Test."""
+    d: dict[str, str] = {"Marc": "red", "Ezri": "red", "Kris": "red"}
+    assert favorite_color(d) == "red"
+
+
+def test_favorite_color_empty() -> None:
+    """Test."""
+    d = dict()
+    assert favorite_color(d) == "no colors found"
+
+
+def test_count_use_case_00() -> None:
+    """Test."""
+    str_list: list[str] = ["h", "a", "a", "b"]
+    return_dict: dict[str, int] = {
+        'h': 1,
+        'a': 2,
+        'b': 1
+    }
+    assert count(str_list) == return_dict
+
+
+def test_count_use_case_01() -> None:
+    """Test."""
+    str_list: list[str] = ["h", "a", "a", "a"]
+    return_dict: dict[str, int] = {
+        'h': 1,
+        'a': 3
+    }
+    assert count(str_list) == return_dict
+
+
+def test_count_empty() -> None:
+    """Test."""
+    str_list: list[str] = []
+    return_dict: dict[str, int] = dict()
+    assert count(str_list) == return_dict
